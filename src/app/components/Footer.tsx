c6532d6e9a7254e2b8ac44f43aca122ab916c334
ltran <loris.tran@epfl.ch>
2025-01-02 18:52:47 +0100

    feat: modified footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,16 +1,44 @@
-// components/Footer.js
+// src/components/Footer.tsx
+import BSALogo from '../images/hero/BSALogo';
+import { FaTelegramPlane, FaTwitter, FaEnvelope } from 'react-icons/fa';
+
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-base-content">
-      <div>
-        <p>Conference Inc.<br />Providing reliable tech since 2024</p>
-      </div>
-      <div>
-        <span className="footer-title">Social</span>
-        <div className="grid grid-flow-col gap-4">
-          <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.56v14.91c0 2.52-2.04 4.56-4.56 4.56H4.56C2.04 24 0 21.96 0 19.47V4.56C0 2.04 2.04 0 4.56 0h14.91C21.96 0 24 2.04 24 4.56zM9.6 19.2V9.6H6.4v9.6h3.2zm-1.6-10.8c1.04 0 1.92-.88 1.92-1.92s-.88-1.92-1.92-1.92-1.92.88-1.92 1.92.88 1.92 1.92 1.92zm12 10.8v-5.6c0-3.04-1.6-4.4-3.76-4.4-1.76 0-2.56.96-3.04 1.6v-1.36H9.6v9.6h3.2v-5.2c0-1.36.24-2.72 1.92-2.72 1.6 0 1.6 1.76 1.6 2.8v5.12h3.2z"/></svg></a>
-          <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.56v14.91c0 2.52-2.04 4.56-4.56 4.56H4.56C2.04 24 0 21.96 0 19.47V4.56C0 2.04 2.04 0 4.56 0h14.91C21.96 0 24 2.04 24 4.56zM9.6 19.2V9.6H6.4v9.6h3.2zm-1.6-10.8c1.04 0 1.92-.88 1.92-1.92s-.88-1.92-1.92-1.92-1.92.88-1.92 1.92.88 1.92 1.92 1.92zm12 10.8v-5.6c0-3.04-1.6-4.4-3.76-4.4-1.76 0-2.56.96-3.04 1.6v-1.36H9.6v9.6h3.2v-5.2c0-1.36.24-2.72 1.92-2.72 1.6 0 1.6 1.76 1.6 2.8v5.12h3.2z"/></svg></a>
-          <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.56v14.91c0 2.52-2.04 4.56-4.56 4.56H4.56C2.04 24 0 21.96 0 19.47V4.56C0 2.04 2.04 0 4.56 0h14.91C21.96 0 24 2.04 24 4.56zM9.6 19.2V9.6H6.4v9.6h3.2zm-1.6-10.8c1.04 0 1.92-.88 1.92-1.92s-.88-1.92-1.92-1.92-1.92.88-1.92 1.92.88 1.92 1.92 1.92zm12 10.8v-5.6c0-3.04-1.6-4.4-3.76-4.4-1.76 0-2.56.96-3.04 1.6v-1.36H9.6v9.6h3.2v-5.2c0-1.36.24-2.72 1.92-2.72 1.6 0 1.6 1.76 1.6 2.8v5.12h3.2z"/></svg></a>
+    <footer className="bg-gradient-to-r from-blue-500 to-purple-600 text-white py-8">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="flex flex-col items-center mb-4 md:mb-0">
+          <span className="text-lg font-bold mb-2">
+            Organized by:
+          </span>
+            <a href="https://bsaepfl.ch/" target="_blank" rel="noopener noreferrer" className="text-2xl">
+                <BSALogo  classname="w-full btn btn-ghost normal-case" />
+            </a>
+        </div>
+        <div className="flex flex-col items-center mb-4 md:mb-0">
+          <span className="text-lg font-bold mb-2">
+            Socials:
+          </span>
+          <div className="flex items-center space-x-4">
+            <a href="https://t.me/+Yg2GqxoXDIIwYWJk" target="_blank" rel="noopener noreferrer" className="text-2xl">
+              <FaTelegramPlane />
+            </a>
+            <a href="https://x.com/bsa_epfl" target="_blank" rel="noopener noreferrer" className="text-2xl">
+              <FaTwitter />
+            </a>
+            <a href="mailto:bsa@epfl.ch" className="text-2xl">
+              <FaEnvelope />
+            </a>
+          </div>
+        </div>
+        <div className="text-center md:text-right">
+          <span className="block text-lg font-bold mb-2">
+            Developed by:
+          </span>
+          <ul className="list-none">
+            <li>Loris</li>
+            <li>Jules</li>
+            <li>Guillaume</li>
+          </ul>
         </div>
       </div>
     </footer>
